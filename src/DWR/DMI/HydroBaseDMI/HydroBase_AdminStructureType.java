c44b365b9ca4c83c73f63573e78c0a62288330b2
Steve Malers <steve.malers@openwaterfoundation.org>
2019-02-09 03:53:43 -0700

    Update code for OpenCDSS open source release, issue #5
    Add copyright/license notice to all source files.
    Update he README file with GPL 3 license.

--- src/DWR/DMI/HydroBaseDMI/HydroBase_AdminStructureType.java
+++ src/DWR/DMI/HydroBaseDMI/HydroBase_AdminStructureType.java
@@ -1,3 +1,27 @@
+// HydroBase_AdminStructureType - Class to store data for HydroBase administrative structure
+// types as returned by usp_CDSS_refStrType_Sel stored procedure.
+
+/* NoticeStart
+
+CDSS HydroBase Database Java Library
+CDSS HydroBase Database Java Library is a part of Colorado's Decision Support Systems (CDSS)
+Copyright (C) 2018-2019 Colorado Department of Natural Resources
+
+CDSS HydroBase Database Java Library is free software:  you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CDSS HydroBase Database Java Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CDSS HydroBase Database Java Library.  If not, see <https://www.gnu.org/licenses/>.
+
+NoticeEnd */
+
 package DWR.DMI.HydroBaseDMI;
 
 import RTi.DMI.DMIDataObject;
--- src/DWR/DMI/HydroBaseDMI/HydroBase_TableModel_WellApplicationView.java
+++ src/DWR/DMI/HydroBaseDMI/HydroBase_TableModel_WellApplicationView.java
@@ -1,3 +1,26 @@
+// HydroBase_TableModel_WellApplicationView - Table Model for a Vector of HydroBase_WellApplicationView objects.
+
+/* NoticeStart
+
+CDSS HydroBase Database Java Library
+CDSS HydroBase Database Java Library is a part of Colorado's Decision Support Systems (CDSS)
+Copyright (C) 2018-2019 Colorado Department of Natural Resources
+
+CDSS HydroBase Database Java Library is free software:  you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CDSS HydroBase Database Java Library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CDSS HydroBase Database Java Library.  If not, see <https://www.gnu.org/licenses/>.
+
+NoticeEnd */
+
 // ----------------------------------------------------------------------------
 // HydroBase_TableModel_WellApplicationView - Table Model for a Vector of 
 //	HydroBase_WellApplicationView objects.
