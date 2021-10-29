742729d276c6f4a9859c63a35bc8aff95b27d3a7
Steve Malers <steve.malers@openwaterfoundation.org>
2021-10-29 11:20:45 -0600

    Fix handling of @require new syntax, issue #39

--- src/DWR/DMI/HydroBaseDMI/HydroBaseDataStore.java
+++ src/DWR/DMI/HydroBaseDMI/HydroBaseDataStore.java
@@ -30,6 +30,7 @@ import RTi.DMI.DMI;
 import RTi.Util.IO.IOUtil;
 import RTi.Util.IO.PropList;
 import RTi.Util.IO.RequirementCheck;
+import RTi.Util.Message.Message;
 import RTi.Util.String.StringUtil;
 import riverside.datastore.DataStoreRequirementChecker;
 
@@ -86,34 +87,48 @@ public HydroBaseDataStore ( String name, String description, DMI dmi, boolean is
  * @return whether the requirement condition is met, from call to check.isRequirementMet()
  */
 public boolean checkRequirement ( RequirementCheck check ) {
+	String routine = getClass().getSimpleName() + ".checkRequirement";
 	// Parse the string into parts:
 	// - calling code has already interpreted the first 3 parts to be able to do this call
 	String requirement = check.getRequirementText();
-	String [] parts = requirement.split(" ");
-	String dataName = parts[3];
-	String operator = parts[4];
-	String checkValue = parts[5];
-	String dbVersion = getVersionForCheck();
-	if ( !dataName.equalsIgnoreCase("version")) {
-		check.setIsRequirementMet(false, "Requirement does not contain keyword 'version' as in '@require datastore " +
-			this.getName() + " version >= YYYYMMDD" );
-		return check.isRequirementMet();
-	}
-	if ( (dbVersion == null) || dbVersion.isEmpty() ) {
-		// Unable to do check.
-		check.setIsRequirementMet(false, "HydroBase database cannot be determined.");
-		return check.isRequirementMet();
-	}
-	else {
-		// HydroBase versions are strings of format YYYYMMDD so can do alphabetical comparison.
-		boolean verCheck = StringUtil.compareUsingOperator(dbVersion, operator, checkValue);
-		String message = "";
-		if ( !verCheck ) {
-			message = "HydroBase version (" + dbVersion + ") does not meet the requirement.";
+	String [] requireParts = requirement.split(" ");
+    String example = "\n  Example: #@require datastore HydroBase version >= 20200720";
+	if ( requireParts.length >= 6 ) {
+		// Have a requirement to check:
+		// - evaluate the parts below
+		String reqProperty = requireParts[3];
+		String operator = requireParts[4];
+		String dbVersion = getVersionForCheck();
+		if ( (dbVersion == null) || dbVersion.isEmpty() ) {
+			// Unable to do check.
+			check.setIsRequirementMet(false, "HydroBase database version cannot be determined.");
+			return check.isRequirementMet();
 		}
-		check.setIsRequirementMet(verCheck, message);
-		return check.isRequirementMet();
+		else if ( reqProperty.equalsIgnoreCase("version") ){
+			// HydroBase versions are strings of format YYYYMMDD so can do alphabetical comparison.
+			String reqVersion = requireParts[5];
+			boolean verCheck = StringUtil.compareUsingOperator(dbVersion, operator, reqVersion);
+			String message = "";
+			if ( !verCheck ) {
+				message = "HydroBase version (" + dbVersion + ") does not meet the requirement: " + operator + " " + reqVersion;
+			}
+			// The following will set to true or false, and fail message if failed.
+			check.setIsRequirementMet(verCheck, message);
+			return check.isRequirementMet();
+		}
+  		else {
+   			String message = "@require datastore property (" + reqProperty + ") is not recognized: " + requirement + example;
+   			check.setIsRequirementMet(false, message);
+   			Message.printWarning(3, routine, message);
+   		}
+   		// If failure here and no message have a coding problem because message needs to be non-empty.
+   		if ( ! check.isRequirementMet() && check.getFailReason().isEmpty() ) {
+   			String message = "@require was not met but have empty fail message - need to fix software.";
+			check.setIsRequirementMet(false, message);
+			Message.printWarning(3, routine, message);
+   		}
 	}
+	return check.isRequirementMet();
 }
 
 /**
